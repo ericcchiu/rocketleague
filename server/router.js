d91e4824b8c5539d81960bbd3ba4c2ce92096c0c
ericcchiu <eric.chn.chiu@gmail.com>
2019-01-26 15:58:52 -0600

    (feat) Add GET route for APOD Nasa.

--- server/router.js
+++ server/router.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const { APOD } = require('./apiRoutes.js');
+const axios = require('axios');
+require('dotenv').config();
+// 'https://api.nasa.gov/planetary/apod?api_key=FsgKdkwyiqWDUyF2ftElb3e2uCNOqZ7p0xGNdD5w'
 
-router.get('/', (req, res) => {
-  res.send('Hello');
+router.get('/api/apod', async (req, res) => {
+  try {
+    const options = {
+      method: 'get',
+      url: APOD + '?api_key=' + process.env.NASA_API_KEY,
+      responseType: 'json'
+    };
+    let entry = await axios(options);
+    res.status(200).json(entry.data);
+  } catch (err) {
+    res.status(200).send('Error retrieving data from NASA API');
+  }
 });
 
-router.post('/', express.json(), (req, res) => {
-});
+// router.post('/', express.json(), (req, res) => {
+// });
 
 module.exports = router;
