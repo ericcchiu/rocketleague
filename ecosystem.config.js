5afc07e930d59753c830728de280d2010fa234bc
ericcchiu <eric.chn.chiu@gmail.com>
2019-01-26 14:04:02 -0600

    (update) Update setup file for pm2 deployment to aws

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,18 @@
 module.exports = {
   apps: [
     {
-      name: 'projectView',
+      name: 'rocketleague',
       script: './server/index.js'
     }
   ],
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-52-15-73-206.us-east-2.compute.amazonaws.com',
-      key: '~/.ssh/projectView.pem',
+      host: 'ec2-18-191-220-238.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/rocketleague.pem',
       ref: 'origin/master',
-      repo: 'git@github.com:ericcchiu/Eric_Services_Projects.git',
-      path: '/home/ubuntu/projectView',
+      repo: 'git@github.com:ericcchiu/rocketleague.git',
+      path: '/home/ubuntu/rocketleague',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
   }
