8ef685b5e3e7a237b1ca2b6050a65add02b1e313
ericcchiu <eric.chn.chiu@gmail.com>
2019-01-26 15:58:17 -0600

    (update) Update server to work with env files.

--- server/index.js
+++ server/index.js
@@ -4,18 +4,19 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const path = require('path');
+const axios = require('axios');
 const app = express();
 require('dotenv').config();
 
-app.use(express.static(path.join(__dirname, '../public/')));
+
 app.use(morgan('dev'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public/')));
 app.use(router);
 
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const server = app.listen(port, console.log(`Server listening on port ${port}`));
 
 module.exports = {
