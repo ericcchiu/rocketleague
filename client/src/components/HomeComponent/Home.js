ab4fa81321425a6e4c2195e2438250396bb0bbe4
ericcchiu <eric.chn.chiu@gmail.com>
2019-02-07 15:02:11 -0600

    (Update) Remove clock component for bug fix causing error messages in console

--- client/src/components/HomeComponent/Home.js
+++ client/src/components/HomeComponent/Home.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import AstronomyCard from './AstronomyCard';
 import MarsWeather from './MarsWeather';
-import Clock from './Clock';
+// import Clock from './Clock';
 
 const Home = (props) => {
   return (
     <div className='HomeBox'>
       <AstronomyCard apodData={props.apodData} />
       <MarsWeather marsData={props.marsData} />
-      <Clock />
+      {/* <Clock /> */}
     </div>
   );
 };
